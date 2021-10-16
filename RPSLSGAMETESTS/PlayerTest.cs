f5f29838ae51b0b2d8b5799c488398c533ad3049
Petra Rábai <petra.rabai@icloud.com>
2021-10-16 13:23:20 +0200

    .

--- RPSLSGAMETESTS/PlayerTest.cs
+++ RPSLSGAMETESTS/PlayerTest.cs
@@ -27,5 +27,45 @@ namespace RPSLSGAMETESTS
 
             Assert.AreEqual(testMenu,player.PlayerChoosedGameMenu);
         }
+
+        [TestCase('P', "Paper")]
+        [TestCase('L', "Lizard")]
+        [TestCase('R', "Rock")]
+        [TestCase('S', "Scissor")]
+        [TestCase('V', "Spock")]
+        [Test]
+        public void CheckPlayerChoosedFromGameCollectionSuccess(char testKey, string testGameItem)
+        {
+            Player player = new Player();
+            GameBoard game = new GameBoard();
+
+            player.PlayerPressedkey = testKey;
+            player.GetChoosedPlayerGameItem(game);
+
+            Assert.AreEqual(testGameItem, player.PlayerChoosedGameItem);
+        }
+
+        [Test]
+        public void CheckSetPlayerWaitForMessageSuccess()
+        {
+            Player player = new Player();
+
+            player.SetPlayerWaitForMessage();
+
+            Assert.Pass("Success");
+        }
+
+        [TestCase(1)]
+        [Test]
+        public void CheckGetPlayerPointSuccess(int testPoint)
+        {
+            Player player = new Player();
+
+            player.GetPlayerPoint();
+
+            Assert.AreNotEqual(testPoint, player.PlayerPoint);
+
+        }
+
     }
 }
